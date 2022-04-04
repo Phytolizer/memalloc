92766b68c45fdcc1627f4c042bbdaea1d976da4c
Kyle Coffey <me@phytolizer.dev>
2022-04-04 12:59:20 -0500

    tweak build script

--- build.c
+++ build.c
@@ -12,6 +12,9 @@ const char* cc(void) {
 
 #define PROJECT_NAME "memalloc"
 #define CFLAGS "-Wall", "-Wextra", "-Wpedantic", "-ggdb3"
+#define LIB_CFLAGS CFLAGS
+#define MAIN_CFLAGS CFLAGS
+#define TEST_CFLAGS CFLAGS, "-I", "tau"
 
 #define TAU_INC "-Itau"
 
@@ -27,13 +30,13 @@ const char* const test_sources[] = {
         "test_main",
 };
 
-#define COMPILE_OBJECTS(sources, extra_flags, objects) \
+#define COMPILE_OBJECTS(sources, flags, objects) \
     do { \
         *(objects) = malloc(sizeof(const char*) * (sizeof(sources) / sizeof(const char*) + 1)); \
         size_t i = 0; \
         FOREACH_ARRAY(const char*, source, sources, { \
             const char* object = PATH("build", "obj", CONCAT(source, ".o")); \
-            CMD(cc(), "-c", CFLAGS, extra_flags, CONCAT(source, ".c"), "-o", object); \
+            CMD(cc(), "-c", flags, CONCAT(source, ".c"), "-o", object); \
             (*(objects))[i] = object; \
             i++; \
         }); \
@@ -46,7 +49,7 @@ int main(int argc, char** argv) {
     MKDIRS("build", "obj");
 
     const char** lib_objects;
-    COMPILE_OBJECTS(lib_sources, "", &lib_objects);
+    COMPILE_OBJECTS(lib_sources, LIB_CFLAGS, &lib_objects);
 
     char** build_lib =
             collect_args("vvvp", "ar", "rcs", "build/lib" PROJECT_NAME ".a", lib_objects);
@@ -54,18 +57,18 @@ int main(int argc, char** argv) {
     coolbuild_exec(build_lib);
 
     const char** main_objects;
-    COMPILE_OBJECTS(main_sources, "", &main_objects);
+    COMPILE_OBJECTS(main_sources, MAIN_CFLAGS, &main_objects);
 
-    char** build_exe = collect_args(
-            "vpvvvv", cc(), main_objects, "-Lbuild", "-lmemalloc", "-o", PATH("build", PROJECT_NAME));
+    char** build_exe = collect_args("vpvvvv", cc(), main_objects, "-Lbuild", "-lmemalloc", "-o",
+            PATH("build", PROJECT_NAME));
     echo_cmd(build_exe);
     coolbuild_exec(build_exe);
 
     const char** test_objects;
-    COMPILE_OBJECTS(test_sources, TAU_INC, &test_objects);
+    COMPILE_OBJECTS(test_sources, TEST_CFLAGS, &test_objects);
 
-    char** build_test_exe = collect_args("vpvvvv", cc(), test_objects, "-Lbuild", "-lmemalloc", "-o",
-            PATH("build", "test_" PROJECT_NAME));
+    char** build_test_exe = collect_args("vpvvvv", cc(), test_objects, "-Lbuild", "-lmemalloc",
+            "-o", PATH("build", "test_" PROJECT_NAME));
     echo_cmd(build_test_exe);
     coolbuild_exec(build_test_exe);
 
