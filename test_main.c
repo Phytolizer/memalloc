b0e6ae5a267305435088e5125f255810fb1101fc
Kyle Coffey <me@phytolizer.dev>
2022-04-04 13:14:38 -0500

    more tests

--- test_main.c
+++ test_main.c
@@ -8,3 +8,17 @@ TEST(memalloc, too_big) {
     void* ptr = memalloc(SIZE_MAX);
     CHECK_NULL(ptr);
 }
+
+TEST(memalloc, ok) {
+    void* ptr = memalloc(1);
+    CHECK_NOT_NULL(ptr);
+    memfree(ptr);
+}
+
+TEST(memalloc, loop) {
+    for (size_t i = 0; i < 1000000; i++) {
+        void* ptr = memalloc(1);
+        CHECK_NOT_NULL(ptr);
+        memfree(ptr);
+    }
+}
